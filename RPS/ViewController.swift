a5fcba944689592392489c61b42af0f2e741b1e6
Marcin Bartminski <mbdjj@o2.pl>
2020-01-07 18:56:22 +0100

    Initial Commit

---
+++ RPS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RPS
+//
+//  Created by Marcin Bartminski on 07/01/2020.
+//  Copyright © 2020 Marcin Bartminski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
